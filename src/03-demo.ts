ece98921d55ad51e23555ea8a72cd4e83ac83fc0
eduardoc <eduardopsy1@gmail.com>
2023-07-02 19:50:38 -0600

    first commit

---
+++ src/03-demo.ts
@@ -0,0 +1,7 @@
+//Para que no se tenga un choque de variables se utiliza una funcion anonima autoejecutada
+(()=>{
+  //myProductName tipo inferido (ya es una variable de strings)
+  let myProductName = 'Product 1';
+  //myProductPrice ya es una variable de tipo number
+  let myProductPrice = 123;
+})();
